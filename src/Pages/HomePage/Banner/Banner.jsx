0e8e947d9404910cfcc469e7ef3e3713363e3571
jerin-arpa <ismatjerinarpa@gmail.com>
2023-12-28 23:28:19 +0600

    change the theme color

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -28,7 +28,7 @@ const Banner = () => {
                             <div className="hero-overlay bg-opacity-80 bg-white"></div>
                             <div className="container mx-auto px-5 flex flex-col lg:flex-row gap-10 py-28 md:py-44 items-center">
                                 <div className="flex-1">
-                                    <h1 className="mb-5 text-3xl lg:text-5xl font-bold">Task <span className="text-info">Management</span> Platform</h1>
+                                    <h1 className="mb-5 text-3xl lg:text-5xl font-bold">Task <span className="text-amber-500">Management</span> Platform</h1>
                                     <p className="mb-5">From concept to applause, FestCon Management conducts remarkable festivals. Our team brings precision, passion, and expertise to every beat, ensuring your festival is a resounding success.</p>
                                     <button className="btn bg-info border-0 text-white hover:bg-white hover:text-info px-14">Lets Explore</button>
                                 </div>
