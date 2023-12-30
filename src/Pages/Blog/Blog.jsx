8d11295d880927f233c4cf4d79f60850595b18cb
jerin-arpa <ismatjerinarpa@gmail.com>
2023-12-30 13:22:17 +0600

    create blog page and make json data for blog

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 
 
 const Blog = () => {
-    return (
-        <div>
+    const [blogs, setBlogs] = useState([]);
 
+    useEffect(() => {
+        fetch('/blog.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
+    return (
+        <div className="bg-gray-100">
+            <div className="container mx-auto px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pt-10 pb-20">
+                {
+                    blogs.map(blog => <div
+                        key={blog.id}
+                        className="relative mb-20">
+                        <img className="rounded-2xl" src={blog.image} alt="" />
+
+                        <div className="bg-white absolute -bottom-20 mx-5 rounded-2xl p-5">
+                            <p>{blog.date}</p>
+                            <h3 className="text-2xl font-bold">{blog.title}</h3>
+                            <p>{blog.description}</p>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
